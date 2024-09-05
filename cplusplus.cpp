ca1b7b4d4c9f29025636132ea307cf90ccee7f70
ElectroWang⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-09-04 20:00:28 -0400

    Create cplusplus.cpp

---
+++ cplusplus.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std; 
+
+
+
+int main() {
+    // Write C++ code here
+    cout << "Try programiz.pro";
+    ma();
+
+    return 0;
+}
+
+char ma() {
+    // Write C++ code here
+    cout << "k";
+
+    return 'i';
+}
+
+float may() {
+    // Write C++ code here
+    cout << "k";
+
+    return 0.0;
+}
+
+int kkk(){cout<<"ooooooooooooo";return 0;}
+
+//https://www.google.com/url?sa=i&url=https%3A%2F%2Fbeginnersbook.com%2F2017%2F08%2Fcpp-functions%2F&psig=AOvVaw3NieUAKY6VW32naRQvIFK5&ust=1725572702313000&source=images&cd=vfe&opi=89978449&ved=0CBcQjhxqFwoTCOj2_t2hqogDFQAAAAAdAAAAABAY
