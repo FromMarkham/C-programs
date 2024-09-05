19b6155c0e02b46e82f38b83d7e69c9265bec452
ElectroWang⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-09-05 19:44:43 -0400

    Create Calling functions.cpp

---
+++ Calling functions.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std; 
+
+//https://www.youtube.com/watch?v=y7GNJlilbvg
+
+float may() {
+    // Write C++ code here
+    cout << "k";
+
+    return 0.0;
+}
+int kkk(){cout<<"ooooooooooooo";return 0;}
+char ma() {
+    // Write C++ code here
+    //cout << "k";
+    cout << "Try programiz.pr";
+    return 'i';
+}
+
+float yyy(){std::cout<<"kkkkkkkkkkk";return 0.0;}
+
+int pp(){std::cout<<"jjjjj";return 0;}
+int main() {
+    // Write C++ code here
+    cout << "Try programiz.pro";
+    ma();
+    may();
+    kkk();
+    yyy();
+    pp();
+    return 0;
+}
+
+
