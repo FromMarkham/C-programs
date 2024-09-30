f26bb36cf92a4108c89db511c8e3941ccde68360
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-09-29 20:09:18 -0400

    Create if statement.cpp

---
+++ if statement.cpp
@@ -0,0 +1,14 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int main() {
+    // Write C++ code here
+    std::cout << "Try programiz.pro";
+    double pop;
+    std::cin>>pop;
+    
+    if(pop>1){
+        std::cout<<"kkkkkkkkkkkkkk"<<std::endl;
+    }
+    return 0;
+}
